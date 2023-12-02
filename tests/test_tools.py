ac57ecb31fdda55f0ce28309e0fe28c292368efa
José Sánchez-Gallego <gallegoj@uw.edu>
2023-12-02 22:11:27 +0000

    Fix tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -9,44 +9,29 @@
 from __future__ import annotations
 
 import pytest
+from pytest_mock import MockFixture
 
-from ln2fill import config
-from ln2fill.tools import close_all, valve_on_off, valves_on_off
+from ln2fill.tools import close_all_valves, valve_on_off
 
 
 async def test_valve_on_off(mock_client_send_command):
-    assert (await valve_on_off("r1")) is True
+    assert (await valve_on_off("r1", True)) is None
 
 
 async def test_valve_on_off_fails(mock_client_send_command, command_failed):
     mock_client_send_command.return_value = command_failed
 
     with pytest.raises(RuntimeError):
-        await valve_on_off("r1")
+        await valve_on_off("r1", True)
 
 
 async def test_valve_on_off_bad_valve():
     with pytest.raises(ValueError):
-        await valve_on_off("a1")
+        await valve_on_off("a1", True)
 
 
-async def test_valves_on_off(mocker):
-    mocker.patch("ln2fill.tools.valve_on_off")
+async def test_close_all_valves(mocker: MockFixture):
+    mm = mocker.patch("ln2fill.tools.valve_on_off")
 
-    valves: list[str] = list(config["valves"])
-
-    operated: list[list[str]] = []
-    async for op in valves_on_off(valves):
-        operated.append(op)
-
-    assert len(operated) == 4
-
-
-async def test_close_all(mocker):
-    mm = mocker.patch("ln2fill.tools.valves_on_off")
-
-    valves: list[str] = list(config["valves"])
-
-    await close_all()
-    mm.assert_called_once()
-    mm.assert_called_with(valves)
+    await close_all_valves()
+    mm.assert_called()
