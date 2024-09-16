13a052342c73a3cac3d94e87aa50055b56e378e4
José Sánchez-Gallego <gallegoj@uw.edu>
2024-09-16 15:16:45 +0000

    Remove old tests and add placeholder and deps

--- tests/test_lvmcryo.py
+++ tests/test_lvmcryo.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # @Author: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Date: 2023-11-10
-# @Filename: conftest.py
+# @Date: 2024-09-16
+# @Filename: test_lvmcryo.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 from __future__ import annotations
+
+from lvmcryo.runner import ln2_runner
+
+
+async def test_lvmcryo():
+    assert ln2_runner
